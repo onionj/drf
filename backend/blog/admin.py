de91059b819b10993df92efb4a91e39c75e885e8
onionj <onionj98@gmail.com>
2021-08-09 00:29:37 +0430

    add article list view

--- backend/blog/admin.py
+++ backend/blog/admin.py
@@ -3,4 +3,11 @@ from .models import Article
 
 # Register your models here.
 
-admin.site.register(Article)
+# admin.site.register(Article)
+
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'status', 'publish')
+    search_fields = ('title', 'author', 'status', 'publish')
+    list_editable = ('status', 'author', 'publish')
--- backend/blog/views.py
+++ backend/blog/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
-
+# from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Article
 # Create your views here.
+
+
+class ArticleList(ListView):
+    def get_queryset(self):
+        return Article.objects.filter(status=True)
