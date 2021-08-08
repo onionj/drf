2b08359aa9ab08230551c66fb3ba8b5c68f08fff
onionj <onionj98@gmail.com>
2021-08-09 01:29:46 +0430

    add api list view

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-
+from rest_framework import views
+from .views import ArticleList
 app_name = 'api'
 
 urlpatterns = [
+    path('', ArticleList.as_view(), name='list')
 ]
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+from rest_framework.generics import ListAPIView
+from .serializer import ArticleSerializers
+from blog.models import Article
+
 
 # Create your views here.
+
+
+class ArticleList(ListAPIView):
+    queryset = Article.objects.all()
+    serializer_class = ArticleSerializers
