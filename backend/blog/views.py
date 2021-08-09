bf8ce17ef212999bf69ee8c46d1f3c0560529d3c
onionj <onionj98@gmail.com>
2021-08-09 21:35:27 +0430

    add detail view to blog

--- backend/blog/views.py
+++ backend/blog/views.py
@@ -1,5 +1,5 @@
-# from django.shortcuts import render
-from django.views.generic import ListView
+from django.shortcuts import get_object_or_404
+from django.views.generic import ListView, DetailView
 from .models import Article
 # Create your views here.
 
@@ -7,3 +7,11 @@ from .models import Article
 class ArticleList(ListView):
     def get_queryset(self):
         return Article.objects.filter(status=True)
+
+
+class ArticleDetail(DetailView):
+    def get_object(self):
+        return get_object_or_404(
+            Article.objects.filter(status=True),
+            pk=self.kwargs.get('pk')
+        )
