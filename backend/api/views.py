99c3af6ad4ef03ce6af48f277604567cc87c9802
onionj <onionj98@gmail.com>
2021-08-12 01:16:37 +0430

    Basic Authentication

--- backend/api/views.py
+++ backend/api/views.py
@@ -2,11 +2,11 @@
 
 # , RetrieveDestroyAPIView, RetrieveAPIView, DestroyAPIView,  ListAPIView
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
-
+from .serializer import ArticleSerializers, UserSerializers
 # from rest_framework.permissions import IsAdminUser # default drf premissions
 from .permissions import IsAuthorOrReadOnly, IsStaffOrReadOnly, IsSuperUserOrStaffReadOnly  # , IsSuperUser
 
-from .serializer import ArticleSerializers, UserSerializers
+# from rest_framework.authentication import SessionAuthentication
 
 from blog.models import Article
 from django.contrib.auth.models import User
@@ -18,6 +18,7 @@ from django.contrib.auth.models import User
 class ArticleList(ListCreateAPIView):
     queryset = Article.objects.all()
     serializer_class = ArticleSerializers
+    # authentication_classes = (SessionAuthentication,)
 
 
 class ArticleDetail(RetrieveUpdateDestroyAPIView):
--- backend/config/settings.py
+++ backend/config/settings.py
@@ -133,5 +133,13 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'api.permissions.IsStaffOrReadOnly',
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        # 'rest_framework.authentication.BasicAuthentication',
+        # 'rest_framework.authentication.SessionAuthentication',
+        # 'rest_framework.authentication.TokenAuthentication',
+        # jwt
+        # knox
+        # oauth
     ]
 }
