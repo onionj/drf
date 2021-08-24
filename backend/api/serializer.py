dfc87725efd9dd58fc8899f04c3f578afd723b4f
onionj <onionj98@gmail.com>
2021-08-25 02:34:40 +0430

    SerializerMethodField for get author data

--- backend/api/serializer.py
+++ backend/api/serializer.py
@@ -4,7 +4,15 @@ from django.contrib.auth import get_user_model
 
 
 class ArticleSerializers(serializers.ModelSerializer):
-    author = serializers.CharField(source="author.username", read_only=True)
+
+    def get_author(self, obj):
+        return {
+            "username": obj.author.username,
+            "first_name": obj.author.first_name,
+            "last_name": obj.author.last_name,
+        }
+
+    author = serializers.SerializerMethodField("get_author")
 
     class Meta:
         model = Article
