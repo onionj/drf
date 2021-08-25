e59180b0b37a9c096f0aecf5bc2280485f14f16c
onionj <onionj98@gmail.com>
2021-08-25 16:08:20 +0430

    exclude password and email in UserSerializers

--- backend/api/serializer.py
+++ backend/api/serializer.py
@@ -38,4 +38,4 @@ class ArticleSerializers(DynamicFieldsMixin, serializers.ModelSerializer):
 class UserSerializers(DynamicFieldsMixin, serializers.ModelSerializer):
     class Meta:
         model = get_user_model()
-        fields = '__all__'
+        exclude = ('password', 'email',)
